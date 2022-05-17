bd69532ae8c32ee08a1f286f31f97098f2e4df9d
anakais <alexandre.saito@outlook.com>
2022-05-17 17:49:24 -0300

    chore: Export Footer to layouts component - clean index unused data

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,16 +5,25 @@
 
     <Nuxt />
 
-
+    <!-- footer -->
+    <main-footer/>
+<!--    <div class="hidden lg:block w-full py-2 bg-black text-center text-xs">-->
+<!--      <p class="container pt-36 text-neutral-300">-->
+<!--        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio impedit praesentium quos. Ab adipisci-->
+<!--        architecto beatae blanditiis debitis.-->
+<!--      </p>-->
+<!--    </div>-->
   </div>
 </template>
 
 <script>
 import navbar from '~/layouts/Navbar'
+import MainFooter from '~/layouts/MainFooter'
 
 export default {
   components: {
-    navbar
+    navbar,
+    MainFooter
   }
 }
 </script>
