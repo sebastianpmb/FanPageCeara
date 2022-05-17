d644d7fd6bdba4779eb192c8f204c44436a97d5a
anakais <alexandre.saito@outlook.com>
2022-05-17 18:01:49 -0300

    chore: Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Ceará FanPage',
+    title: 'Ceará Sporting Club - Nação Alvinegra',
     htmlAttrs: {
       lang: 'pt-BR'
     },
