f73d63c71b0942b31029fb17fe54f41331d92d3b
anakais <alexandre.saito@outlook.com>
2022-05-17 10:24:08 -0300

    Chore: Update structure - Create a default layout - Export navbar to a layout component - Clean unused code from index

---
+++ layouts/Navbar.vue
@@ -0,0 +1,135 @@
+<template>
+  <nav class="relative w-full font-ind bg-cover bg-no-repeat"
+  >
+    <!-- sidebar  -->
+    <transition name="nav">
+      <div
+        v-if="sidebar === true"
+        :key="sidebar"
+        class="fixed inset-0 z-50 overflow-y-auto w-screen h-min-screen bg-gray-200 font-ind"
+      >
+        <div
+          class="w-full mb-24 pt-12 flex flex-col justify-end items-center"
+        >
+          <div class="w-full flex justify-end">
+            <button class="flex content-center mr-12 hover:text-purple-700" @click="closeSideBar">
+              <font-awesome-icon class="fa-2x px-1 -mt-1" icon="xmark"/>
+              <span class="ml-2">Fechar</span>
+            </button>
+          </div>
+          <ul class="mt-12 w-full" @click="closeSideBar">
+            <a href="#planos" class="hover:text-neutral-900">
+              <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300">
+                PROJETO
+              </li>
+            </a>
+            <a href="#setores" class="hover:text-neutral-900">
+              <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300">
+                CAMISA 2020
+              </li>
+            </a>
+            <a href="#parceiros" class="hover:text-neutral-900">
+              <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300">
+                REGULAMENTO
+              </li>
+            </a>
+            <a href="#perguntas" class="hover:text-neutral-900">
+              <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300">
+                FAQ
+              </li>
+            </a>
+            <a href="" class="hover:text-neutral-900">
+              <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300">
+                CLIQUE E VOTE
+              </li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </transition>
+    <section class="relative container flex justify-end items-center h-24 pt-6 px-5 lg:justify-center xl:h-28">
+      <img class="absolute top-0 left-0 w-56 mt-5 -ml-4 xl:w-80 xl:mt-3 xl:-ml-16"
+           src="~/assets/img/logo.png"
+           alt="Logo Nação Alvinegra - Meninas do Vozão"
+      />
+      <ul class="hidden flex-row justify-between items-center lg:flex lg:w-7/12 xl:w-8/12 xl:px-10">
+        <li class="text-pink-300 text-base">
+          <a href="#planos">PROJETO</a>
+        </li>
+        |
+        <li class="text-base">
+          <a href="#setores">CAMISA 2020</a>
+        </li>
+        |
+        <li class="text-base">
+          <a href="#parceiros">REGULAMENTO</a>
+        </li>
+        |
+        <li class="text-base">
+          <a href="#perguntas">FAQ</a>
+        </li>
+        |
+        <li class="text-base">
+          <a href="">CLIQUE E VOTE</a>
+        </li>
+        <div class="flex absolute right-0 mr-8">
+          <div class="ml-5 text-right font-sans text-xs md:ml-8 lg:text-sm">
+            <p class="uppercase font-extrabold">John Doe</p>
+            <span class="text-pink-300 font-light"><a href="">EDITAR</a> | <a href="">SAIR</a></span>
+          </div>
+          <button class="relative w-7 -mt-1 ml-3 fill-zinc-900 hover:fill-purple-700">
+            <div
+              class="absolute top-0 right-0 w-5 -mt-3 -mr-3.5 rounded-full bg-purple-700 text-sm text-black lg:-mt-2"
+            >
+              1
+            </div>
+            <div
+              class="w-8 h-8 rounded-full bg-white border border-black overflow-hidden lg:w-9 lg:h-9 xl:w-11 xl:h-11"
+            >
+              <font-awesome-icon class="fa-2x mt-2 text-gray-600" icon="user"/>
+            </div>
+          </button>
+        </div>
+      </ul>
+
+      <!-- mobile buttons -->
+      <div class="flex items-center lg:hidden">
+        <button class="hover:text-purple-700" @click="openSideBar">
+          <font-awesome-icon class="fa-2x px-1" icon="bars"/>
+        </button>
+        <div class="ml-5 text-right font-sans text-xs md:ml-8">
+          <p class="uppercase font-extrabold">John Doe</p>
+          <span class="text-pink-300 font-light"><a href="">EDITAR</a> | <a href="">SAIR</a></span>
+        </div>
+        <button class="relative w-7 -mt-1 ml-3 fill-zinc-900 hover:fill-purple-700">
+          <div class="absolute top-0 right-0 w-5 -mt-3 -mr-3.5 rounded-full bg-purple-700 text-sm text-black">
+            1
+          </div>
+          <div class="w-8 h-8 rounded-full bg-white border border-black overflow-hidden">
+            <font-awesome-icon class="fa-2x mt-2 text-gray-600" icon="user"/>
+          </div>
+        </button>
+      </div>
+    </section>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'navbar',
+  data () {
+    return {
+      sidebar: false,
+    }
+  },
+  methods: {
+    openSideBar () {
+      this.sidebar = true
+      document.body.classList.add('scroll-lock')
+    },
+    closeSideBar () {
+      document.body.classList.remove('scroll-lock')
+      this.sidebar = false
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- navbar on top -->
+    <navbar class="relative z-10" />
+
+    <Nuxt />
+
+
+  </div>
+</template>
+
+<script>
+import navbar from '~/layouts/Navbar'
+
+export default {
+  components: {
+    navbar
+  }
+}
+</script>
