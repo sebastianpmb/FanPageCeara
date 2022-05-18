d008f610bfd4eeee8674edff09ceafb2aecd3c1f
anakais <alexandre.saito@outlook.com>
2022-05-18 12:01:50 -0300

    [navbar] update links and profile texts

--- layouts/Navbar.vue
+++ layouts/Navbar.vue
@@ -20,18 +20,18 @@
           <ul class="mt-12 w-full" @click="closeSideBar">
             <nuxt-link to="/" class="hover:text-neutral-900">
               <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300"
-                  :class="{'text-pink-300' : $route.path === '/'}"
+                  :class="{'text-pink-300' : $route.fullPath === '/'}"
               >
                 PROJETO
               </li>
             </nuxt-link>
-            <a href="#camisa" class="hover:text-neutral-900">
+            <nuxt-link to="/#camisa" class="hover:text-neutral-900">
               <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300"
-                  :class="{'text-pink-300' : $route.path === '/camisa'}"
+                  :class="{'text-pink-300' : $route.fullPath === '/#camisa'}"
               >
                 CAMISA 2020
               </li>
-            </a>
+            </nuxt-link>
             <nuxt-link to="/regulamento" class="hover:text-neutral-900">
               <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300"
                   :class="{'text-pink-300' : $route.path === '/regulamento'}"
@@ -46,7 +46,7 @@
                 FAQ
               </li>
             </nuxt-link>
-            <a href="https://nacaoalvinegra2.fanbase.com.br/" class="hover:text-neutral-900">
+            <a href="https://nacaoalvinegra2.fanbase.com.br/" target="_blank" class="hover:text-neutral-900">
               <li class="py-6 pl-8 border-b-2 border-neutral-700 hover:bg-purple-700 transition duration-300"
                   :class="{'text-pink-300' : $route.path === '/votacao'}"
               >
@@ -58,17 +58,19 @@
       </div>
     </transition>
     <section class="relative container flex justify-end items-center h-24 pt-6 px-5 lg:justify-center xl:h-28">
-      <img class="absolute top-0 left-0 w-56 mt-5 -ml-4 xl:w-80 xl:mt-3 xl:-ml-16"
-           src="~/assets/img/logo.png"
-           alt="Logo Nação Alvinegra - Meninas do Vozão"
-      />
+      <nuxt-link to="/">
+        <img class="absolute top-0 left-0 w-56 mt-5 -ml-4 xl:w-80 xl:mt-3 xl:-ml-16"
+             src="~/assets/img/logo.png"
+             alt="Logo Nação Alvinegra - Meninas do Vozão"
+        />
+      </nuxt-link>
       <ul class="hidden flex-row justify-between items-center lg:flex lg:w-7/12 xl:w-8/12 xl:px-10">
-        <li class="text-base" :class="{'text-pink-300' : $route.path === '/'}">
+        <li class="text-base" :class="{'text-pink-300' : $route.fullPath === '/'}">
           <nuxt-link to="/">PROJETO</nuxt-link>
         </li>
         |
-        <li class="text-base" :class="{'text-pink-300' : $route.path === '/camisa'}">
-          <a href="#camisa">CAMISA 2020</a>
+        <li class="text-base" :class="{'text-pink-300' : $route.fullPath === '/#camisa'}">
+          <nuxt-link to="/#camisa">CAMISA 2020</nuxt-link>
         </li>
         |
         <li class="text-base" :class="{'text-pink-300' : $route.path === '/regulamento'}">
@@ -80,18 +82,19 @@
         </li>
         |
         <li class="text-base" :class="{'text-pink-300' : $route.path === '/votacao'}">
-          <a href="https://nacaoalvinegra2.fanbase.com.br/">CLIQUE E VOTE</a>
+          <a href="https://nacaoalvinegra2.fanbase.com.br/" target="_blank">CLIQUE E VOTE</a>
         </li>
         <div class="flex absolute right-0 mr-8">
           <div class="ml-5 text-right font-sans text-xs md:ml-8 lg:text-sm">
-            <p class="uppercase font-extrabold">John Doe</p>
-            <span class="text-pink-300 font-light"><a href="">EDITAR</a> | <a href="">SAIR</a></span>
+            <p class="uppercase font-extrabold">Vozão ID</p>
+            <span class="text-pink-300 font-light"><a href="">CRIAR CONTA</a></span>
           </div>
           <button class="relative w-7 -mt-1 ml-3 fill-zinc-900 hover:fill-purple-700">
             <div
+              v-if="profileNotify"
               class="absolute top-0 right-0 w-5 -mt-3 -mr-3.5 rounded-full bg-purple-700 text-sm text-black lg:-mt-2"
             >
-              1
+              {{ profileNotify }}
             </div>
             <div
               class="w-8 h-8 rounded-full bg-white border border-black overflow-hidden lg:w-9 lg:h-9 xl:w-11 xl:h-11"
@@ -108,15 +111,16 @@
           <font-awesome-icon class="fa-2x px-1" icon="bars"/>
         </button>
         <div class="ml-5 text-right font-sans text-xs md:ml-8">
-          <p class="uppercase font-extrabold">John Doe</p>
-          <span class="text-pink-300 font-light"><a href="">EDITAR</a> | <a href="">SAIR</a></span>
+          <p class="uppercase font-extrabold">Vozão ID</p>
+          <span class="text-pink-300 font-light"><a href="">CRIAR CONTA</a></span>
         </div>
         <button class="relative w-7 -mt-1 ml-3 fill-zinc-900 hover:fill-purple-700">
-          <div class="absolute top-0 right-0 w-5 -mt-3 -mr-3.5 rounded-full bg-purple-700 text-sm text-black">
-            1
+          <div v-if="profileNotify"
+               class="absolute top-0 right-0 w-5 -mt-3 -mr-3.5 rounded-full bg-purple-700 text-sm text-black">
+            {{ profileNotify }}
           </div>
           <div class="w-8 h-8 rounded-full bg-white border border-black overflow-hidden">
-            <font-awesome-icon class="fa-2x mt-2 text-gray-600" icon="user"/>
+            <img src="~/assets/img/profileDefaultImage.png" alt="Perfil padrão">
           </div>
         </button>
       </div>
@@ -129,6 +133,7 @@ export default {
   data () {
     return {
       sidebar: false,
+      profileNotify: 0,
     }
   },
   methods: {
