3dd7c44de0a70a090cfaa8b3c976c1c01a372870
anakais <alexandre.saito@outlook.com>
2022-05-19 20:20:01 -0300

    [navbar] fix: z-index above footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <!-- navbar on top -->
-    <navbar class="bg-cover bg-no-repeat z-10" />
+    <navbar class="bg-cover bg-no-repeat z-20" />
 
     <Nuxt />
 
     <!-- footer -->
-    <lazy-main-footer/>
+    <lazy-main-footer class="z-10"/>
   </div>
 </template>
 
