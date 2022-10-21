dc47948be147523596b84e21f5ce4e5cef36a46a
anakais <alexandre.saito@outlook.com>
2022-10-21 10:24:50 -0300

    chore (mainFooter): update layouts shadows

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -16,7 +16,7 @@
       </section>
       <!-- address and social-->
       <section class="bg-white -mt-6 md:flex md:flex-row-reverse">
-        <article class="relative container pt-20 md:w-1/2 md:pl-0 md:-ml-12 md:pt-16 lg:pt-28">
+        <div class="relative container pt-20 md:w-1/2 md:pl-0 md:-ml-12 md:pt-16 lg:pt-28">
           <p class="text-xs mb-6">
             <strong>Ceará Sporting Club</strong><br>
             Avenida João Pessoa, 3532 - Porangabuçu - Ceará - CEP 60425-813 <br>
@@ -24,9 +24,9 @@
             <font-awesome-icon :icon="['fab', 'whatsapp']"/>
             (85) 3052-7777
           </p>
-        </article>
+        </div>
         <!-- social -->
-        <article class="bg-white overflow-y-hidden md:w-10/12">
+        <div class="bg-white overflow-y-hidden md:w-10/12">
           <div id="footer-social"
                class="bg-contain bg-no-repeat w-10/12 max-w-xs -mb-12 pt-16 pb-16 md:w-full c-social-links">
             <div class="flex text-sm mb-12 md:mb-10 md:pt-2">
@@ -77,22 +77,16 @@
               <img class="-mt-1 ml-2 w-20" src="~/static/img/fanbase.png" alt="Logo Fanbase">
             </div>
           </div>
-        </article>
+        </div>
       </section>
-<!--      <div class="w-full py-2 bg-black text-center text-xs">-->
-<!--        <p class="container text-neutral-300">-->
-<!--          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio impedit praesentium quos. Ab adipisci-->
-<!--          architecto beatae blanditiis debitis.-->
-<!--        </p>-->
-<!--      </div>-->
     </div>
 
     <!-- footer desk -->
     <div class="hidden lg:block lg:relative">
       <section class="w-10/12 h-full -mt-6 flex flex-row-reverse items-end float-right xl:w-11/12 2xl:w-full">
-        <img class="relative xl:h-56 xl:w-full 2xl:h-64" src="~/static/img/footer-black-bg.png" alt="Footer background 2">
+        <img class="relative xl:h-56 xl:w-full 2xl:h-64 drop-shadow-[0_15px_35px_rgba(0,0,0,0.25)]" src="~/static/img/footer-black-bg.png" alt="Footer background 2">
         <div class="absolute top-0 w-full">
-          <article class="pt-7 flex justify-end xl:pt-16 2xl:pt-24">
+          <div class="pt-7 flex justify-end xl:pt-16 2xl:pt-24">
             <div class="w-5/12 pl-12 pr-20 xl:pl-32 xl:pr-12">
               <p class="text-xs xl:pt-2 2xl:text-sm">
                 <strong>Ceará Sporting Club</strong><br>
@@ -104,7 +98,7 @@
               </p>
             </div>
             <div class="relative w-1/3 pr-6 flex justify-between items-center xl:pr-12 xl:w-5/12">
-              <img class="absolute w-56 right-0 top-0 -mt-16 mr-6 px-1 shadow-xl
+              <img class="absolute w-56 right-0 top-0 -mt-16 mr-6 px-1 shadow-xl drop-shadow-[0_15px_35px_rgba(0,0,0,0.25)]
                           xl:w-6/12 xl:-mt-24 2xl:-mt-36 2xl:mr-10 2xl:px-0"
                    src="~/static/img/logo-NacaoAlvinegra-white.webp"
                    alt="Logo Nação Alvinegra"
@@ -113,10 +107,10 @@
               <img class="w-24 xl:w-28 2xl:w-36" src="~/static/img/footer-logo-socio.png" alt="Logo Sócio Vozão">
               <img class="w-24 xl:w-28 2xl:w-36" src="~/static/img/footer-logo-time.png" alt="Logo Vozão Time do Povo">
             </div>
-          </article>
+          </div>
         </div>
         <!-- social -->
-        <article class="absolute left-0 max-w-xs pr-4 xl:pr-0">
+        <div class="absolute left-0 max-w-xs pr-4 xl:pr-0">
           <div class="bg-contain bg-no-repeat w-full">
             <img class="relative" src="~/static/img/footer-purple-bg.png" alt="Footer background">
             <div class="absolute top-0 w-full">
@@ -169,7 +163,7 @@
               </div>
             </div>
           </div>
-        </article>
+        </div>
       </section>
     </div>
   </footer>
